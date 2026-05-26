a86f8b85c817c94469a174360317b6ce43aa4030
Arunkv52 <arunkbva5@gmail.com>
2026-05-26 18:17:22 +0530

    file add on

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
+  plugins: [react()],
+    server: {
     host:true,
     port: 5173
   },
@@ -16,4 +13,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
