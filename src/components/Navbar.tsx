05e5553a223e69a10b98522f99c6eccf564bd9f8
Arunkv52 <arunkbva5@gmail.com>
2026-06-23 10:25:35 +0530

    file addon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Menu, X, ChevronDown } from 'lucide-react'
 
-import Logo from '@/assets/logo/Logo.png'
+import Logo from '../assets/logo/Logo.png'
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false)
